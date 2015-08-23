66a0df1197881de52be311b95b4d9f810b9753e4
mgahsan <mgahsan@gmail.com>
2015-08-23 03:17:21 -0600

    FINAL SUBMISSION FOR ASSIGNMENT: cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,19 +2,26 @@
 
 ## Introduction:
 
-## The following user defined function-pair produce and cache 
-## the inverse of an invertible (det != 0) square matrix (n x n). 
+## This kind of programming exercise is useful to address time
+## consuming computations in R. Computing inverse of a matrix
+## can be costly when it needs to be done mutliple times when
+## solving for a linear system or even when using it in a loop.
+## The following user defined function-pair produces the inverse 
+## of an invertible (det != 0) square matrix (n x n) and then
+## caches it in memory for future use.
 
-## Making the Cache Matrix:
+## The Cache Matrix:
 
 ## The following function creates a special 'matrix' object which 
-## takes a matrix 'x' as an argument and then creates an environment 
-## that enables both caching and retrieving the inverse of the input 
-## matrix.  
+## takes a invertible matrix 'x' as an argument and then creates an 
+## environment that enables both caching and retrieving the inverse 
+## of the input matrix.  
 
-## The function creates a list of 4 other functions in the global 
-## environment/ user's workspace which provies following 
-## functionality:
+## The function `makeCacheMatrix' is defined in the global environment 
+## (workspace). A list of 4 other functions has been created inside it
+## for which the defining environment is the inside of the host function.
+## The parent for this child environment is the global environment. 
+## These 4 functions provide the following functionality:
 
 ## 1. Set the value of the matrix - `set`
 ## 2. Get the value of the matrix - `get`
@@ -24,14 +31,19 @@
 makeCacheMatrix <- function(x = matrix()) {
        
         IM <- NULL
+        
+        ## Creating the subfunctions
+        
         set <- function(y) {
                 x <<- y
                 IM <<- NULL
         }
-        
         get <- function() x
         setinv <- function(inv) IM <<- inv
         getinv <- function() IM
+        
+        ## Storing the functions into a list
+
         list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
@@ -41,13 +53,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## returned by `makeCacheMatrix` function above. Using an if-else 
 ## control structure, it first checks to see if the inverse matrix 
 ## has already been produced. If so, then, it skips the computation 
-## and gets the inverse matrix from the cache via the `getinv`. 
-## Otherwise, it computes the inverse of the original matrix 'x' 
-## and stores the output in cache memory via the `setinv`.
+## and returns the inverse matrix from the cache via `getinv` function. 
+## Otherwise, it gets the data on the original matrix 'x' via 'get', 
+## computes the inverse of 'x' using 'solve' function, stores the 
+## output in cache memory via `setinv` function, and then prints 
+## the output.
+
+##`cachesolve` is also defined in the global environment (workspace).
 
 cacheSolve <- function(x, ...) {
-        
-        ## Return a matrix that is the inverse of 'x'
         
         IM <- x$getinv()
         
@@ -62,13 +76,57 @@ cacheSolve <- function(x, ...) {
         IM
 }
 
+
+## Scoping Rules:
+
+## R will create a temporary environment inside of 'makeCacheMatrix'
+## function everytime the function will be called. For any function
+## or a free variable inside that environment, R will first try to
+## find a value inside the environment itself and then step up to its
+## parent environment which, in this case, is the global environment.
+## For this script under discussion, everything other than the `solve`
+## function is defined within the above two enviroments. For the solve
+## function the defining environment (top-level) is the base package.
+## Generally, if the search reaches the global environment and a value 
+## could not be found, R will then search through its search list until
+## it reaches the empty environment. Each item in the search list is 
+## an environment. The search list is designed in a way that each item
+## is followed by its parent environment. The base environment is the
+## last item in R search list and its parent is the empty environment. 
+## Empty environment does not have a parent. Global, Base and Empty 
+## are system environments in R.
+
+## Use the following R functions to check for environments:
+
+## ls(), ls.str() - lists objects inside an environment
+## environment() - returns the name of the environment 
+## parent.env() - returns the parent environment of an environment
+
+
+## Example: if a special matrix 'M' is created by the makeCacheMatrix` 
+## function then:
+
+## ls(environment(makeCacheMatrix): "makeCacheMatrix","cacheSolve","M"
+## ls(environment(M$get): "set", "get", "setinv", "getinv", "IM", "x"
+## environment(makeCacheMatrix): <environment: R_GlobalEnv>
+## environment(M$get): <environment: 0x0000000007658c40> 
+## parent.env(environment(M$get)): <environment: R_GlobalEnv>
+## Parent.env(baseenv()): <environment: R_EmptyEnv>
+
+## Evaluation Algorithm:
+
+## 1. Run a matrix input though 'makeCacheMatrix' and store the
+##    special matrix in an R object
+## 2. Run the R object through 'cacheSolve' to cache and print
+##    the inverse matrix
+## 3. Retrieve the cached data using 'cacheSolve' again
+
 ## A Technical Notes:
 
 ## The inverse of a singular matrix, a matrix that has a determinant
-## of zero (0), cannot be calculated. Inverse of a singular matrix is 
-## not defined. R will produce the error, "system is exactly singular." 
-## in such cases. It is advisable to use a non-singular matrix for 
-## evaluation this assignment.
+## of zero (0), is not defined and cannot be computed. R will produce
+## the error, "system is exactly singular" in such cases. It is 
+## advisable to use a non-singular matrix for evaluation this assignment.
 
 ## Further Reading and Evaluation:
 
@@ -76,6 +134,6 @@ cacheSolve <- function(x, ...) {
 ## script has been uploaded under the same repository in both PDF 
 ## and Markdown formats. The document discusses topics like Matrix
 ## Inversion, 'Solve' Function in R, Implication of Scoping Rules,
-## Test Outputs and Evaluation etc. The document is uploaded by 
-## the name of "SolutionManual." Please put a star on the repo
-## if you like it. THANKS !
+## How the system of functions work, Test Outputs and Evaluation etc. 
+## The document is uploaded by the name of "SolutionManual." Please 
+## put a star on the repo if you like it. THANKS !
